69d9ff628b8409a6a1b3d6b245dd6da4ad2e1213
Sergey Karpynskyi <karpinskiy94@gmail.com>
2021-02-01 13:51:50 +0200

    hw8(modal)

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
+    
   },
   {
     preview:
--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 import gallery from "./gallery.js"
 
 const eleRef = document.querySelector('.js-gallery');
+const boxRef = document.querySelector('.js-lightbox');
+const modalImgRef = document.querySelector('.lightbox__image');
+const modalOvrRef = document.querySelector('.lightbox__overlay');
+
 
 const gallRef = items => {
-   const imgRef = items.map(item => {
+    const imgRef = items.map(item => {
         const list = document.createElement('li');
         list.classList.add('gallery__item');
         const link = document.createElement('a');
@@ -14,10 +18,11 @@ const gallRef = items => {
         img.setAttribute('src', item.preview);
         img.setAttribute('data-source', item.original);
         img.setAttribute('alt', item.description);
-
+       
         list.appendChild(link);
         list.appendChild(img)
-        
+         
+       
         return list;
     })
     
@@ -32,6 +37,31 @@ function clickImg(event) {
     if (event.target.nodeName !== 'IMG') {
         return
     }
-    console.log(event.target.dataset.source);
+    boxRef.classList.add('is-open');
+    const butCloseRef = document.querySelector('.lightbox__button');
+    butCloseRef.addEventListener('click', closeModal);
+    
+    function closeModal(event) {
+        boxRef.classList.remove('is-open');
+        modalImgRef.srcset = '';
+        
+    }
+    
+    modalOvrRef.addEventListener('click', closeModalOvr);
+    function closeModalOvr(event) {
+        boxRef.classList.remove('is-open');
+    }
+   
+    window.addEventListener('keydown', closeModalKey);
+    function closeModalKey(event) {
+        if (event.code === "Escape") {
+            boxRef.classList.remove('is-open');
+      } 
+    }
+
+    modalImgRef.srcset = event.target.dataset.source;
 
 }
+ 
+
+   
